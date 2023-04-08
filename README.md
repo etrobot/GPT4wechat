44dde5840c9587154e26860eb3037b3beef7925d
franklin <etrobot@outlook.com>
2023-04-09 02:35:01 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GPT4wechat
 将new bing的gpt-4和微信结合形成私人助理
+
+步骤：
+首先你需要登录有new bing资格的账号，使用cookie editor导出cookies.json保存到项目目录；
+然后运行pip install -r requirements.txt安装所需要的库，最后运行gpt4wechat.py即可。
---
+++ gpt4wechat.py
@@ -0,0 +1,76 @@
+# encoding:utf-8
+from concurrent.futures import ThreadPoolExecutor
+import itchat
+from itchat.content import *
+from EdgeGPT import Chatbot, ConversationStyle
+import asyncio,logging,json
+
+@itchat.msg_register([TEXT, SHARING])
+def handler_single_msg(msg):
+    weChat().handle(msg)
+    return None
+
+@itchat.msg_register([TEXT, SHARING], isGroupChat=True)
+def handler_group_msg(msg):
+    weChat().handle_group(msg)
+    return None
+
+class weChat():
+    def __init__(self):
+        with open('./cookies.json', 'r') as f:
+            cookies = json.load(f)
+            self.bot = Chatbot(cookies=cookies, proxy="http://127.0.0.1:1080")
+        pass
+
+    def startup(self):
+        # login by scan QRCode
+        itchat.auto_login(hotReload=True)
+        # start message listener
+        itchat.run()
+
+    def handle(self, msg):
+        thread_pool.submit(self._do_send, msg['Text'])
+
+    def handle_group(self, msg):
+        if not msg['IsAt']:
+            return
+        query = msg['Content'][len(msg['ActualNickName']) + 1:]
+        if query is not None:
+            thread_pool.submit(self._do_send_group, query, msg)
+
+    def send(self, msg, receiver):
+        itchat.send(msg, toUserName=receiver)
+
+    def _do_send(self, query):
+        if query == '':
+            return
+        try:
+            reply_text = self.reply(query)
+            if reply_text is not None:
+                self.send('[Bing]' + reply_text)
+        except Exception as e:
+            log.exception(e)
+
+    def _do_send_group(self, query, msg):
+        if not query:
+            return
+        group_id = msg['User']['UserName']
+        reply_text = self.reply(query)
+        if reply_text is not None:
+            self.send('@' + msg['ActualNickName'] + ' ' + reply_text.strip(), group_id)
+
+    def reply(self,queryText):
+        reply_text=None
+        reply = asyncio.run(self.bot.ask(prompt=queryText, conversation_style=ConversationStyle.creative,
+                                         wss_link="wss://sydney.bing.com/sydney/ChatHub"))
+        if reply:
+            reply_text = reply["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]
+        return reply_text
+
+if __name__=='__main__':
+    log = logging.getLogger('itchat')
+    log.setLevel(logging.DEBUG)
+    thread_pool = ThreadPoolExecutor(max_workers=8)
+    wechat = weChat()
+    wechat.startup()
+
