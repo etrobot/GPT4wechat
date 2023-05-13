ac9cb86b162f9b376e1cd05924eb650ac04c3684
Frank Lin <etrobot@outlook.com>
2023-05-13 17:40:34 +0800

    Update gpt4wechat.py

--- gpt4wechat.py
+++ gpt4wechat.py
@@ -17,7 +17,7 @@ def handler_group_msg(msg):
 
 class weChat():
     def __init__(self):
-        self.bot = asyncio.run(Chatbot.create(cookie_path='./cookies.json',proxy="http://127.0.0.1:7890")
+        self.bot = asyncio.run(Chatbot.create(cookie_path='./cookies.json',proxy="http://127.0.0.1:7890"))
 
     def startup(self):
         # login by scan QRCode
@@ -58,7 +58,7 @@ class weChat():
 
     def reply(self,queryText):
         reply_text=None
-        reply = asyncio.run(self.bot.ask(prompt=queryText, conversation_style=ConversationStyle.precise,
+        reply = asyncio.run(self.bot.ask(prompt=queryText, conversation_style=ConversationStyle.balanced,
                                          wss_link="wss://sydney.bing.com/sydney/ChatHub"))
         if reply:
             reply_text = reply["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]
